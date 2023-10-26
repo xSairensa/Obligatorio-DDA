5c37ac4bfc063c17374ddee3e50c54b34ad2a0ce
DanielaFranca <118018092+DanielaFranca@users.noreply.github.com>
2023-10-26 12:52:19 -0300

    Actualizo menus

--- src/Main.java
+++ src/Main.java
@@ -53,206 +53,183 @@ public class Main {
         keyboard.close();
     }
 
+    private static void limpiarConsola() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     //region Metodos de Gestion
 
     public static void gestionJugadores(Scanner keyboard){
-        boolean volver = false;
-        while(volver == false){
-            System.out.println("Elija una opción:\n" +
-                    "1- Ingresar Jugador\n" +
-                    "2- Modificar Jugador\n" +
-                    "3- Eliminar Jugador\n" +
-                    "4- Lista de Jugadores\n" +
-                    "0- Volver");
-            try {
-                short opcion = keyboard.nextShort();
-                switch (opcion) {
-                    case 1:
-                        altaJugador(keyboard);
-                        break;
-                    case 4:
-                        listarJugadores();
-                        break;
-                    case 0:
-                        volver = true;
-                        break;
-                    default:
-                        System.out.println("La opcion ingresada no es correcta");
-                        break;
-                }
-            }
-            catch (InputMismatchException e){
-                String ingreso = keyboard.nextLine();
-                System.err.println("Debe ingresar un numero, no '" + ingreso + "'");
+        limpiarConsola();
+        Scanner keyboardJ = new Scanner(System.in);
+        boolean salir = false;
+        while(salir == false){
+            System.out.println("Ingrese la opcion deseada\n" +
+                    "1- Alta Jugador \n" +
+                    "2- Modificar Jugador \n" +
+                    "3- Eliminar Jugador \n" +
+                    "4- Listar Jugadores \n" +
+                    "5- Actualizar Jugadores \n" +
+                    "0- Salir");
+            //     try {
+            short opcion2 = keyboardJ.nextShort();
+            switch (opcion2) {
+                case 1:
+                    altaJugador(keyboardJ);
+                    break;
+                case 2:
+                    modificarJugador(keyboardJ);
+                    break;
+                case 3:
+                    eliminarJugador(keyboardJ);
+                    break;
+                case 4:
+                    listarJugadores();
+                    break;
+                case 5:
+                    actualizarJugadores();
+                    break;
+                case 0:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("La opcion ingresada no es correcta");
+                    break;
             }
         }
+        keyboardJ.close();
     }
 
     public static void gestionTecnicos(Scanner keyboard){
-        boolean volver = false;
-        while(volver == false){
-            System.out.println("Elija una opción:\n" +
-                    "1- Ingresar Técnico\n" +
-                    "2- Modificar Técnico\n" +
-                    "3- Eliminar Técnico\n" +
-                    "4- Lista de Técnicos\n" +
-                    "0- Volver");
-            try {
-                short opcion = keyboard.nextShort();
-                switch (opcion) {
-                    case 1:
-                        System.out.println("Test");
-                        break;
-                    case 0:
-                        volver = true;
-                        break;
-                    default:
-                        System.out.println("La opcion ingresada no es correcta");
-                        break;
-                }
-            }
-            catch (InputMismatchException e){
-                String ingreso = keyboard.nextLine();
-                System.err.println("Debe ingresar un numero, no '" + ingreso + "'");
+        limpiarConsola();
+     //   Scanner keyboardT = new Scanner(System.in);
+        boolean salir = false;
+        while(salir == false){
+            System.out.println("Ingrese la opcion deseada\n" +
+                    "1- Alta Técnico \n" +
+                    "2- Modificar Técnico \n" +
+                    "3- Eliminar Técnico \n" +
+                    "4- Listar Técnicos \n" +
+                    "5- Actualizar Técnicos \n" +
+                    "0- Salir");
+            //     try {
+            short opcion3 = keyboard.nextShort();
+            switch (opcion3) {
+                case 1:
+                    altaTecnico(keyboard);
+                    break;
+                case 2:
+                    modificarTecnico(keyboard);
+                    break;
+                case 3:
+                    eliminarTecnico(keyboard);
+                    break;
+                case 4:
+                    listarTecnicos();
+                    break;
+                case 5:
+                    actualizarTecnicos();
+                    break;
+                case 0:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("La opcion ingresada no es correcta");
+                    break;
             }
         }
+        keyboard.close();
     }
 
     public static void gestionArbitros(Scanner keyboard){
-        boolean volver = false;
-        while(volver == false){
-            System.out.println("Elija una opción:\n" +
-                    "1- Ingresar Árbitro\n" +
-                    "2- Modificar Árbitro\n" +
-                    "3- Eliminar Árbitro\n" +
-                    "4- Lista de Árbitros\n" +
-                    "0- Volver");
-            try {
-                short opcion = keyboard.nextShort();
-                switch (opcion) {
-                    case 1:
-                        System.out.println("Test");
-                        break;
-                    case 0:
-                        volver = true;
-                        break;
-                    default:
-                        System.out.println("La opcion ingresada no es correcta");
-                        break;
-                }
-            }
-            catch (InputMismatchException e){
-                String ingreso = keyboard.nextLine();
-                System.err.println("Debe ingresar un numero, no '" + ingreso + "'");
+        limpiarConsola();
+        //   Scanner keyboardT = new Scanner(System.in);
+        boolean salir = false;
+        while(salir == false){
+            System.out.println("Ingrese la opcion deseada\n" +
+                    "1- Alta Árbitro \n" +
+                    "2- Modificar Árbitro \n" +
+                    "3- Eliminar Árbitro \n" +
+                    "4- Listar Árbitros \n" +
+                    "5- Actualizar Árbitros \n" +
+                    "0- Salir");
+            //     try {
+            short opcion4 = keyboard.nextShort();
+            switch (opcion4) {
+                case 1:
+                    altaArbitro(keyboard);
+                    break;
+                case 2:
+                    modificarArbitro(keyboard);
+                    break;
+                case 3:
+                    eliminarArbitro(keyboard);
+                    break;
+                case 4:
+                    listarArbitros();
+                    break;
+                case 5:
+                    actualizarArbitros();
+                    break;
+                case 0:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("La opcion ingresada no es correcta");
+                    break;
             }
         }
+        keyboard.close();
     }
 
     public static void gestionarCuadros(Scanner keyboard){
-        boolean volver = false;
-        while(volver == false){
-            System.out.println("Elija una opción:\n" +
-                    "1- Ingresar Cuadro\n" +
-                    "2- Modificar Modificar Cuadro\n" +
-                    "3- Eliminar Cuadro\n" +
-                    "4- Lista de Cuadros\n" +
-                    "5- Detalle del Cuadro" +
-                    "0- Volver");
-            try {
-                short opcion = keyboard.nextShort();
-                switch (opcion) {
-                    case 1:
-                        System.out.println("Test");
-                        break;
-                    case 0:
-                        volver = true;
-                        break;
-                    default:
-                        System.out.println("La opcion ingresada no es correcta");
-                        break;
-                }
-            }
-            catch (InputMismatchException e){
-                String ingreso = keyboard.nextLine();
-                System.err.println("Debe ingresar un numero, no '" + ingreso + "'");
-            }
-        }
+        limpiarConsola();
+        System.out.println("Ingresar Datos:");
+        new Scanner(System.in).nextLine();
     }
 
     public static void gestionarPartidos(Scanner keyboard){
-        boolean volver = false;
-        while(volver == false){
-            System.out.println("Elija una opción:\n" +
-                    "1- Ingresar Partido\n" +
-                    "2- Detalle del Partido\n" +
-                    "3- Cálculo de Salario" +
-                    "0- Volver");
-            try {
-                short opcion = keyboard.nextShort();
-                switch (opcion) {
-                    case 1:
-                        System.out.println("Test");
-                        break;
-                    case 0:
-                        volver = true;
-                        break;
-                    default:
-                        System.out.println("La opcion ingresada no es correcta");
-                        break;
-                }
-            }
-            catch (InputMismatchException e){
-                String ingreso = keyboard.nextLine();
-                System.err.println("Debe ingresar un numero, no '" + ingreso + "'");
-            }
-        }
+        limpiarConsola();
+        System.out.println("Ingresar Datos:");
+        new Scanner(System.in).nextLine();
     }
 
     //endregion
-
     //region Metodos Jugador
-
     public static void altaJugador(Scanner keyboard){
-        //Cuando usas keyboard.nextInt(), se deja un salto de línea pendiente en el búfer
-        //Hay que consumirlo con la linea siguiente sino rompera el programa
-        keyboard.nextLine();
-
-        System.out.println("Ingrese el nombre: ");
-        String pNombre = keyboard.nextLine();
-        System.out.println("Ingrese el apellido: ");
-        String pApellido = keyboard.nextLine();
-        System.out.println("Ingrese el documento: ");
-        String pCi = keyboard.nextLine();
-        System.out.println("Ingrese el sueldo: ");
-        int pSueldo = keyboard.nextInt();
-
-        keyboard.nextLine();
-
-        System.out.println("Ingrese la posición: ");
-        String pPosicion = keyboard.nextLine();
-
-        Jugador pJugador = new Jugador(pNombre, pApellido, pCi, pSueldo, pPosicion);
-        long cantidad = pListaJugadores.size();
-        pListaJugadores.add(pJugador);
-        if(pListaJugadores.size() > cantidad){
-            System.out.println("Jugador agregado correctamente");
-        }
-        else{
-            System.out.println("Error al guardar el jugador");
-        }
-
     }
-
+    public static void modificarJugador(Scanner keyboard){
+    }
+    public static void eliminarJugador(Scanner keyboard){
+    }
     public static void listarJugadores(){
-        if (pListaJugadores.size() > 0) {
-            for (Jugador pJugador : pListaJugadores) {
-                System.out.println(pJugador.toString());
-            }
-        }
-        else{
-            System.out.println("No hay Jugadores ingresados");
-        }
     }
-
+    public static void actualizarJugadores(){
+    }
+    //endregion
+    //region Metodos Tecnico
+    public static void altaTecnico(Scanner keyboard){
+    }
+    public static void modificarTecnico(Scanner keyboard){
+    }
+    public static void eliminarTecnico(Scanner keyboard){
+    }
+    public static void listarTecnicos(){
+    }
+    public static void actualizarTecnicos(){
+    }
+    //endregion
+    //region Metodos Arbitro
+    public static void altaArbitro(Scanner keyboard){
+    }
+    public static void modificarArbitro(Scanner keyboard){
+    }
+    public static void eliminarArbitro(Scanner keyboard){
+    }
+    public static void listarArbitros(){
+    }
+    public static void actualizarArbitros(){
+    }
     //endregion
 }
